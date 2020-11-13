2663ec041b3254c521faa4b31088b45290fa070d
LeulAria <leulsegedtariku@gmail.com>
2020-11-13 17:13:08 +0300

    added instruction and language_code 🤳️

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const TeleBot = require('telebot');
 const axios = require('axios')
+const { language_code, instructions } = require('./util/util')
 require('dotenv').config()
 
 // init
@@ -7,25 +8,35 @@ const bot = new TeleBot({
     token: process.env.TELEGRAM_TOKEN
 })
 
-bot.on(["/start", "/hello"], (msg) => {
+bot.on(["/start"], (msg) => {
     console.log(msg);
     bot.sendMessage(msg.from.id, `Hello ${msg.chat.username}`);
 })
 
+bot.on("/get_language_code", (msg) => {
+    bot.sendMessage(msg.from.id, language_code)
+})
+
+bot.on("/get_instructions", (msg) => {
+    bot.sendMessage(msg.from.id, instructions)
+})
+
 bot.on('text', async (msg) => {
-    const text = msg.text.split(':').map((s) => s.trim());
-    const textToTrans = text[0];
-    const from = text[1] || 'en';
-    const to = text[2] || 'am';
+    if(msg.text !== '/get_language_code' && msg.text !== '/start' && msg.text !== '/get_instructions') {
+        const text = msg.text.split(':').map((s) => s.trim());
+        const textToTrans = text[0];
+        const from = text[1] || 'en';
+        const to = text[2] || 'am';
 
-    const res = await axios.get(`https://translated-mymemory---translation-memory.p.rapidapi.com/api/get?langpair=${from}|${to}&q=${textToTrans}!&mt=1&onlyprivate=0&de=a@b.c`, {
-        headers: {
-            "x-rapidapi-key": process.env.RAPID_API_KEY,
-            "x-rapidapi-host": process.env.RAPID_API_HOST
-        }
-    })
+        const res = await axios.get(`https://translated-mymemory---translation-memory.p.rapidapi.com/api/get?langpair=${from}|${to}&q=${textToTrans}!&mt=1&onlyprivate=0&de=a@b.c`, {
+            headers: {
+                "x-rapidapi-key": process.env.RAPID_API_KEY,
+                "x-rapidapi-host": process.env.RAPID_API_HOST
+            }
+        })
 
-    msg.reply.text(res.data.responseData.translatedText)
+        msg.reply.text(res.data.responseData.translatedText)
+    }
 })
 
 bot.start();
--- util/util.js
+++ util/util.js
@@ -1,6 +1,124 @@
-const lngDetector = new (require('languagedetect'));
+const language_code = `
+Afrikaans: AF
+Amharic: AM
+Arabic: AR
+Azerbaijani: AZ
+Belarusian: BE
+Bulgarian: BG
+Bengali: BN
+Tibetan: BO
+Basque: EU
+Persian: FA
+Finnish: FI
+Faroese: FO
+French: FR
+Western Frisian: FY
+Irish: GA
+Gaelic, Scottish Gaelic: GD
+Galician: GL
+Gujarati: GU
+Hausa: HA
+Hebrew: HE
+Hindi: HI
+Haitian, Haitian Creole: HT
+Hungarian: HU
+Armenian: HY
+Indonesian: ID
+Igbo: IG
+Indonesian: ID
+Icelandic: IS
+Italian: IT
+Hebrew: HE
+Japanese: JA
+Yiddish: YI
+Georgian: KA
+Kazakh: KK
+Central Khmer: KM
+Kannada: KN
+Korean: KO
+Kirghiz, Kyrgyz: KY
+Latin: LA
+Luxembourgish, Letzeburgesch: LB
+Lao: LO
+Lithuanian: LT
+Latvian: LV
+Malagasy: MG
+Marshallese: MH
+Maori: MI
+Macedonian: MK
+Malayalam: ML
+Mongolian: MN
+Moldavian, Moldovan: RO
+Marathi: MR
+Malay: MS
+Maltese: MT
+Burmese: MY
+Norwegian Bokmål: NB
+Nepali: NE
+Dutch, Flemish: NL
+Norwegian Nynorsk: NN
+Norwegian: NO
+Chichewa, Chewa, Nyanja: NY
+Polish: PL
+Pashto, Pushto: PS
+Portuguese: PT
+Quechua: QU
+Rundi: RN
+Romanian, Moldavian, Moldovan: RO
+Russian: RU
+Sindhi: SD
+Sango: SG
+Sinhala, Sinhalese: SI
+Slovak: SK
+Slovenian: SL
+Samoan: SM
+Shona: SN
+Somali: SO
+Albanian: SQ
+Serbian: SR
+Southern Sotho: ST
+Sundanese: SU
+Swedish: SV
+Swahili: SW
+Tamil: TA
+Telugu: TE
+Tajik: TG
+Thai: TH
+Tagalog: TL
+Tonga (Tonga Islands): TO
+Turkish: TR
+Tatar: TT
+Ukrainian: UK
+Urdu: UR
+Uzbek: UZ
+Vietnamese: VI
+Wolof: WO
+Xhosa: XH
+Yiddish: YI
+Yoruba: YO
+Chinese: ZH
+Zulu: ZU
+`;
 
-module.exports = () => {
-    const d = lngDetector.detect('this is sheet');
-    return d
-}
+const instructions = `
+How to translate on G-Translate Bot
+By default if you wright english and send it will translate it to amharic
+
+how to translate to other languages:
+
+Syntax:
+<Your text>  :  <en> : <am>
+
+Description:
+<Your text> here put your text to translate it can be in any of available languages
+check available languages   /get_language_code 
+
+then put :  
+<en>  is the language code you have written your's
+: then put another : to split to which language you want to translate
+<am>  in this case am is amharic
+`;
+
+
+module.exports.language_code = language_code;
+module.exports.instructions = instructions;
