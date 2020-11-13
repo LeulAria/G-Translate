93ae53de53b46be6d4b70ea5effb9a6cc31b8a6b
LeulAria <leulsegedtariku@gmail.com>
2020-11-13 15:33:11 +0300

    idel err fix test

--- app.js
+++ app.js
@@ -29,3 +29,11 @@ bot.on('text', async (msg) => {
 })
 
 bot.start();
+
+
+var http = require('http');
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World!');
+    res.end();
+}).listen(process.env.PORT);
