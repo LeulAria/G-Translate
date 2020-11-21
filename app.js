fb279ac007e8c899ae4e97f8661e38a876711d2e
LeulAria <leulsegedtariku@gmail.com>
2020-11-21 11:52:54 +0300

    added deta deploy

--- app.js
+++ app.js
@@ -45,6 +45,6 @@ bot.start();
 var http = require('http');
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
+    res.write('G Tralate @gtranslator1_bot!');
     res.end();
 }).listen(process.env.PORT);
